cd52bca5519dfc16d3928c1fe82f1eae0989d0bc
Giruba Beulah SE <gibeulah@microsoft.com>
2019-05-02 01:08:52 +0530

    Add project files.

---
+++ MaxIndex[j-i]_Efficient/ArrayUtility.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxIndex_j_i__Efficient
+{
+    public static class ArrayUtility
+    {
+        public static int[] GetArrayFromUser() {
+            int[] array = null;
+
+            Console.WriteLine("Enter the number of elements in the array");
+            try{
+                int arrayLength = int.Parse(Console.ReadLine().Trim());
+                Console.WriteLine("Enter the elements of the array separated by " +
+                    "space, comma or semi-colon");
+                String[] elements = Console.ReadLine().Trim().Split(' ', ',', ';');
+                array = new int[arrayLength];
+                for (int index = 0; index < arrayLength; index++) {
+                    array[index] = int.Parse(elements[index]);
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+
+            return array;
+        }
+
+        public static int MaximumIndexInTheArray(int[] array) {
+            int arrayLength = array.Length;
+            int maxIndexDifference = int.MinValue;
+
+            int[] minElementsHolder = new int[arrayLength];
+            int[] maxElementsHolder = new int[arrayLength];
+
+            minElementsHolder[0] = array[0];
+            for (int leftToRightIndex = 1; leftToRightIndex < arrayLength; leftToRightIndex++) {
+                minElementsHolder[leftToRightIndex] = Math.Min(minElementsHolder[leftToRightIndex - 1], array[leftToRightIndex]);
+            }
+
+            maxElementsHolder[arrayLength - 1] = array[arrayLength - 1];
+            for (int rightToLeftIndex = arrayLength - 2; rightToLeftIndex >= 0; rightToLeftIndex--) {
+                maxElementsHolder[rightToLeftIndex] = Math.Max(array[rightToLeftIndex], maxElementsHolder[rightToLeftIndex+1]);
+            }
+
+            int leftIndex = 0;
+            int rightIndex = 0;
+            while (leftIndex < arrayLength && rightIndex < arrayLength) {
+                if (minElementsHolder[leftIndex] < maxElementsHolder[rightIndex])
+                {
+                    //LeftElement < rightElement -> Calculate maxIndexDifference
+                    maxIndexDifference = Math.Max(maxIndexDifference, (rightIndex-leftIndex));
+                    //Check whether the next right element is also greater
+                    rightIndex++;
+                }
+                else {
+                    leftIndex++;
+                }
+            }
+            return maxIndexDifference;
+        }
+    }
+}
---
+++ MaxIndex[j-i]_Efficient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MaxIndex_j_i__Efficient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum index [j-i] such that element at " +
+                "j is greater than element at i!");
+            Console.WriteLine("-----------------------------------------" +
+                "-------------------------------");
+
+            int[] array = ArrayUtility.GetArrayFromUser();
+            Console.WriteLine("The maximum index is "+ArrayUtility.MaximumIndexInTheArray(array));
+            Console.ReadLine();
+
+        }
+    }
+}
